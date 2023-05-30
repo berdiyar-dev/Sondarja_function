9a6552b43ee50781c4986abf70c95c2ce416165c
berdiyar-desktop <berdiyare@gmail.com>
2023-05-31 00:42:07 +0500

    user1

--- sondarja.py
+++ sondarja.py
@@ -1,5 +1,5 @@
 def param(n1, n2):
     return n1 ** n2
 num1, num2 = map(int, input("Son va darajaga Kiriting: . . .").split())
-number = param(num1, num2)
-print(number)
+numbers = param(num1, num2)
+print(numbers)
